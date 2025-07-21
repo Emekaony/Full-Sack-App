be71d40685e9001c21692df1d95d953fed442dea
Nnaemeka <Nnaemekaonyeokoro@outlook.com>
2025-07-21 01:08:02 -0700

    Learning a lotta new web stuff

--- index.js
+++ index.js
@@ -1,36 +1,9 @@
-class Person {
-  // in modern js, this is how u make a field private and inaccessible!
-  #ID = "";
-  constructor(firstName, lastName, age, id) {
-    this.firstName = firstName;
-    this.lastName = lastName;
-    this.age = age;
-    this.#ID = id;
-  }
+document.getElementById("container").addEventListener("click", () => {
+  console.log("The container event took off!!");
+});
 
-  // simple member functions
-  // regular javascript is very limited
-  greetPerson() {
-    const greeting = `Hello there ${this.firstName} ${this.lastName}`;
-    console.log(greeting);
-  }
-
-  canGoClubbing() {
-    let msg;
-    const cango = this.age > 20;
-
-    if (!cango) {
-      msg = "Sorry, ur too young";
-    } else {
-      msg = "have fun partying, be safe!";
-    }
-    console.log(msg);
-  }
-}
-
-const app = () => {
-  const Emeka = new Person("Nnaemeka", "Onyeokoro", 24);
-  console.log(Emeka.firstName);
-};
-
-app();
+document.getElementById("child").addEventListener("click", (e) => {
+  // this makes it so that the event doesn't get bubbled up to the parent
+  e.stopPropagation();
+  console.log("The child event took off!!");
+});
