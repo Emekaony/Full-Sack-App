54a7165d6d5f8ed70767d28b4010c3ea5fe82357
Nnaemeka <Nnaemekaonyeokoro@outlook.com>
2025-08-05 07:43:22 -0700

    Working on this course fr now

--- index.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("container").addEventListener("click", () => {
-  console.log("The container event took off!!");
-});
-
-document.getElementById("child").addEventListener("click", (e) => {
-  // this makes it so that the event doesn't get bubbled up to the parent
-  e.stopPropagation();
-  console.log("The child event took off!!");
-});
